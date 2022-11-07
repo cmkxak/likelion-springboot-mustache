839eb59ada71449fa510a0ef5f6aacc8a9c8f1e3
cmkxak <gcm466@naver.com>
2022-11-07 15:30:29 +0900

    기본키 자동 생성 전략 IDENTITY로 설정

--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -1,19 +1,17 @@
 package com.mustache.bbs.domain.entity;
 
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 
 @Entity
 @NoArgsConstructor
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
     private String contents;
 
