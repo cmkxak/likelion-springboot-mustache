6eb7e020803d6d8b3f329aa6417f87abfaacff95
cmkxak <gcm466@naver.com>
2022-11-07 14:32:29 +0900

    articles dto 클래스 추가

---
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -0,0 +1,15 @@
+package com.mustache.bbs.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class ArticleDto {
+    private Long id;
+    private String title;
+    private String contents;
+
+}
