d89b995c0b861ce67f77e1d520a90031fc7c6a68
cmkxak <gcm466@naver.com>
2022-11-07 15:24:28 +0900

    db에 Article Object 저장하도록 수정

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.dto.ArticleDto;
+import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.service.ArticleService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/articles")
-@Controller
+@AllArgsConstructor
 @Slf4j
+@Controller
 public class ArticleController {
+    private ArticleService articleService;
+
 
     @GetMapping("/new")
     public String articles(){
@@ -20,6 +26,8 @@ public class ArticleController {
     @PostMapping("/posts")
     public String createArticle(ArticleDto articleDto){
         log.info(articleDto.toString());
+        Article article = articleDto.toEntity();
+        articleService.save(article);
         return "";
     }
 }
