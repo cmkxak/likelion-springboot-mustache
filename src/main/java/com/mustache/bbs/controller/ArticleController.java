85100ed9a6b6ce832c8dd19be59d13d8aa21507d
cmkxak <gcm466@naver.com>
2022-11-08 15:46:37 +0900

    JPA를 활용한 findAll(), findById() 구현

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -6,9 +6,13 @@ import com.mustache.bbs.service.ArticleService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/articles")
 @AllArgsConstructor
@@ -17,10 +21,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ArticleController {
     private ArticleService articleService;
 
+    @GetMapping("")
+    public String index(){
+        return "redirect:/articles/list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Article> articles = articleService.findAll();
+        model.addAttribute("articles", articles);
+        return "articles/list";
+    }
 
     @GetMapping("/new")
     public String articles(){
         return "articles/new";
+    }
+
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model){
+        Optional<Article> optArticle = articleService.findById(id);
+
+        if(optArticle.isPresent()){
+            Article article = optArticle.get();
+            model.addAttribute("article", article);
+            return "articles/show";
+        }else{
+            return "articles/error";
+        }
     }
 
     @PostMapping("/posts")
--- src/main/java/com/mustache/bbs/service/ArticleService.java
+++ src/main/java/com/mustache/bbs/service/ArticleService.java
@@ -5,12 +5,23 @@ import com.mustache.bbs.repository.ArticleRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @AllArgsConstructor
 @Service
 public class ArticleService {
     private ArticleRepository articleRepository;
 
+    public Optional<Article> findById(Long id){
+        return articleRepository.findById(id);
+    }
+
     public void save(Article article){
         articleRepository.save(article);
+    }
+
+    public List<Article> findAll() {
+        return articleRepository.findAll();
     }
 }
