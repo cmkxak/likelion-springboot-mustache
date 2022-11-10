824b3fd1f61a717df5b2e7269ea2fa527b29fbf0
cmkxak <gcm466@naver.com>
2022-11-10 14:33:57 +0900

    getter 어노테이션 추가

--- src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -2,8 +2,10 @@ package com.mustache.bbs.domain.dto;
 
 import com.mustache.bbs.domain.entity.Article;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.ToString;
 
+@Getter
 @AllArgsConstructor
 @ToString
 public class ArticleDto {
