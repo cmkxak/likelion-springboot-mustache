a3f769469a0d3b374cd71b8649992fc4eba6aecf
cmkxak <gcm466@naver.com>
2022-11-10 11:52:01 +0900

    불필요한 어노테이션 제거

--- src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -2,10 +2,8 @@ package com.mustache.bbs.domain.dto;
 
 import com.mustache.bbs.domain.entity.Article;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.ToString;
 
-@Getter
 @AllArgsConstructor
 @ToString
 public class ArticleDto {
