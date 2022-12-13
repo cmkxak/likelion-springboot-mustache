708273631dbacdda0229cb7d36e3d8988459c5b0
cmkxak <gcm466@naver.com>
2022-12-13 15:25:19 +0900

    [refactor] : 기본 생성자 추가

--- src/main/java/com/mustache/bbs/domain/dto/user/UserLoginRequest.java
+++ src/main/java/com/mustache/bbs/domain/dto/user/UserLoginRequest.java
@@ -2,7 +2,9 @@ package com.mustache.bbs.domain.dto.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class UserLoginRequest {
